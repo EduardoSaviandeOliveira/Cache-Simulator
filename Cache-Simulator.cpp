1796c99d127adc40c4bf68b7b20b237f46e1e231
Eduardo-Savian-de-Oliveira <eduardosaviandeoliveira@proton.me>
2022-10-20 16:09:36 -0300

    Language changed and structure changed

--- Cache-Simulator.cpp
+++ Cache-Simulator.cpp
@@ -1,81 +1,76 @@
 #include <iostream>
 #include <fstream>
-#include <time.h>
 
-using namespace std;
+struct Block {
+    int nset = 0;
+};
+
+struct CacheEntry {
+    int* value = nullptr;
+};
+
+void printStatus(Block* cache, int memorySize, int numBlocks, int numSets, int numHits, int numMisses){
+    std::cout << "Memory Size: " << memorySize << std::endl;
+    std::cout << "Number of Blocks: " << numBlocks << std::endl;
+    std::cout << "Number of Sets: " << numSets << std::endl;
+    std::cout << "Number of Hits: " << numHits << std::endl;
+    std::cout << "Number of Misses: " << numMisses << std::endl;
+    std::cout << "Hit Ratio: " << (float)numHits / (float)(numHits + numMisses) << std::endl;
+    std::cout << "Miss Ratio: " << (float)numMisses / (float)(numHits + numMisses) << std::endl;
+    std::cout << "Cache: " << std::endl;
+    for (int i = 0; i < numBlocks; i++) {
+        std::cout << cache[i].nset << " ";
+    }
+    std::cout << std::endl;
+}
 
 bool powerOfTwo(int x){
 	return x * (!(x & (x - 1)));
 }
 
-int setarNumeroDeBlocos() {
-    int tamanhoDaCache = 0;
-
-    while(!powerOfTwo(tamanhoDaCache)) {
-        cout << "Digite o número de blocos da cache: ";
-        cin >> tamanhoDaCache;
-    }
-    
-    return tamanhoDaCache;
-}
-
-int setarNumeroDeSets() {
-    int tamanhoDaCache = 0;
-
-    while(!powerOfTwo(tamanhoDaCache)) {
-        cout << "Digite o número de sets do bloco: ";
-        cin >> tamanhoDaCache;
-    }
-    
-    return tamanhoDaCache;
-}
-
-int setarTamanhoDaMemoria() {
-    int tamanhoDaMemoria = 0;
-
-    while(!powerOfTwo(tamanhoDaMemoria)) {
-        cout << "Digite o tamanho da memória: ";
-        cin >> tamanhoDaMemoria;
-    }
-    
-    return tamanhoDaMemoria;
-}
-
-int* freshMem(int n){
-	int* array = new int[n];
-	for(int i = 0; i < n; i++) array[i] = 0;
+int* freshMemory(int size){
+	int* array = new int[size];
+	for(int i = 0; i < size; i++) array[i] = 0;
 	return array;
 }
 
-void lerArquivo(int* memoria, int tamanhoDaMemoria) {
-    FILE* arquivo = fopen("t4.txt", "r");
+int* loadFileToMemory(int memorySize){
+    int* memory = freshMemory(memorySize);
+	FILE* file = fopen("memory.txt", "r");
 
-    if (arquivo == NULL) {
-        cout << "Erro ao abrir o arquivo" << endl;
-        return;
-    }
+	if (file == NULL) {
+		std::cerr << "Unable to open the file" << std::endl;
+		return memory;
+	}
 
-    for (int i = 0; i < tamanhoDaMemoria; i++) {
-        fscanf(arquivo, "%d", &memoria[i]);
-    }
-
-    fclose(arquivo);
+	for (int i = 0; i < memorySize; i++) 
+        fscanf(file, "%d", &memory[i]);
+	
+	fclose(file);
+    return memory;
 }
 
+Block* createCache(int numBlocks, int numSets) {
+    Block* cache = new Block[numBlocks];
 
-void simularCache(int numeroDeBlocos, int tamanhoDaMemoria, int numeroDeSets) {
+    return cache;
+}
+
+void simulateCacheDirectMapped(int memorySize, int numBlocks, int numSets) {
+    int* memory = loadFileToMemory(memorySize);
+    Block* cache = createCache(numBlocks, numSets);
+
+    int numHits = 0;
+    int numMisses = 0;
+
+    printStatus(cache, memorySize, numBlocks, numSets, numHits, numMisses);
     
-    
+    delete[] cache;
+    delete[] memory;
 }
 
 int main() {
-    srand(time(NULL));
-
-    int numeroDeBlocos = setarNumeroDeBlocos();
-    int tamanhoDaMemoria = setarTamanhoDaMemoria();
-    int numeroDeSets = setarNumeroDeSets();
-
-    simularCache(numeroDeBlocos, tamanhoDaMemoria, numeroDeSets);
-                                                                                                                                                                                                                                                                                                                                                                                           
+     simulateCacheDirectMapped(5, 4, 1);
+                                                                                                                                                                                                                                                                                                                             
     return 0;
 }
