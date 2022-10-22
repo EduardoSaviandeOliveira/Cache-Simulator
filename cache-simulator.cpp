914a70881108d8c4cf35a1235862aa0d8a006a9b
Eduardo-Savian-de-Oliveira <eduardosaviandeoliveira@proton.me>
2022-10-22 15:07:12 -0300

    Indentation and functions have been reworked

--- cache-simulator.cpp
+++ cache-simulator.cpp
@@ -1,63 +1,113 @@
 #include <iostream>
 #include <fstream>
 
-struct Set {
+struct Set
+{
     int value = 0;
-    int* addr = nullptr;
+    int *addr = nullptr;
     int timeAcess = 0;
+    bool dirtyBit = false;
 };
 
-struct Block {
-    Set* sets = nullptr;
+struct Block
+{
+    Set *sets = nullptr;
 };
 
-bool powerOfTwo(int x){
+void printCache(Block *cache, int numBlocks, int numSets, int memorySize, int numHits)
+{
+    std::cout << "Cache ";
+
+    if (numBlocks == 1)
+        std::cout << "direct mapping" << std::endl;
+    else if (numSets == 1)
+        std::cout << "fully associative" << std::endl;
+    else
+        std::cout << "set associative" << std::endl;
+
+    std::cout << "\nBlocos\t\tSets\n";
+
+    int numDirtyBit = 0;
+
+    for (int i = 0; i < numBlocks; i++)
+    {
+        std::cout << i << "\t\t";
+        for (int j = 0; j < numSets; j++)
+        {
+            std::cout << cache[i].sets[j].value << "    ";
+            if (cache[i].sets[j].dirtyBit)
+                numDirtyBit++;
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "\nMemory size:" << memorySize << "\n";
+    std::cout << "Hits: " << numHits << "\n";
+    std::cout << "Misses: " << memorySize - numHits << "\n"; // num of misses
+    std::cout << "Hit Rate: " << (double)numHits / (memorySize)*100 << "%\n";
+    std::cout << "Dirty Bits: " << numDirtyBit << "\n\n";
+}
+
+bool powerOfTwo(int x)
+{
     return x * (!(x & (x - 1)));
 }
 
-int* freshMemory(int size) {
-	int* array = new int[size];
-	for (int i = 0; i < size; i++) array[i] = 0;
-	return array;
-}
-
-int countFile() {
+int countFile()
+{
     std::ifstream file("data.txt");
     int count = 0;
     int temp;
-    while (file >> temp) count++;
+    while (file >> temp)
+        count++;
     file.close();
     return count;
 }
 
-int* loadFileToMemory(int memorySize) {
-    int* memory = freshMemory(memorySize);
-	FILE* file = fopen("data.txt", "r");
+int *freshMemory(int size)
+{
+    int *array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = 0;
+    return array;
+}
 
-	if (file == NULL) {
-		std::cerr << "Unable to open the file" << std::endl;
-		return memory;
-	}
+int *loadFileToMemory(int memorySize)
+{
+    int *memory = freshMemory(memorySize);
+    FILE *file = fopen("data.txt", "r");
 
-	for (int i = 0; i < memorySize; i++) 
+    if (file == NULL)
+    {
+        std::cerr << "Unable to open the file" << std::endl;
+        return memory;
+    }
+
+    for (int i = 0; i < memorySize; i++)
         fscanf(file, "%d", &memory[i]);
-	
-	fclose(file);
+
+    fclose(file);
     return memory;
 }
 
-Block* createCache(int numBlocks, int numSets) {
-    Block* cache = new Block[numBlocks];
+Block *createCache(int numBlocks, int numSets)
+{
+    Block *cache = new Block[numBlocks];
     for (int i = 0; i < numBlocks; i++)
         cache[i].sets = new Set[numSets];
     return cache;
 }
 
-Block* sortCache (Block* cache, int numBlocks, int numSets) {
-    for (int i = 0; i < numBlocks; i++) {
-        for (int j = 0; j < numSets; j++) {
-            for (int k = 0; k < numSets - 1; k++) {
-                if (cache[i].sets[k].timeAcess > cache[i].sets[k + 1].timeAcess) {
+Block *sortCache(Block *cache, int numBlocks, int numSets)
+{
+    for (int i = 0; i < numBlocks; i++)
+    {
+        for (int j = 0; j < numSets; j++)
+        {
+            for (int k = 0; k < numSets - 1; k++)
+            {
+                if (cache[i].sets[k].timeAcess > cache[i].sets[k + 1].timeAcess)
+                {
                     Set temp = cache[i].sets[k];
                     cache[i].sets[k] = cache[i].sets[k + 1];
                     cache[i].sets[k + 1] = temp;
@@ -68,65 +118,48 @@ Block* sortCache (Block* cache, int numBlocks, int numSets) {
     return cache;
 }
 
-bool isCacheValue (Block* cache, int blockIndex, int numSets, int memoryValue, int setNumber) {
-    for (int i = 0; i < numSets; i++) {
-        if (cache[blockIndex].sets[i].value == memoryValue) {
-            cache[blockIndex].sets[i].timeAcess = setNumber;
+bool isCacheValue(Block *cache, int blockIndex, int numSets, int memoryValue, int timeAcess)
+{
+    for (int i = 0; i < numSets; i++)
+    {
+        if (cache[blockIndex].sets[i].value == memoryValue)
+        {
+            cache[blockIndex].sets[i].timeAcess = timeAcess;
             return true;
         }
     }
-    return false;   
+    return false;
 }
 
-void printCache(Block* cache, int numBlocks, int numSets, int memorySize, int numHits) {
-    std::cout << "Cache ";
-    
-    if (numBlocks == 1)
-        std::cout << "direct mapping" << std::endl;
-    else if (numSets == 1)
-        std::cout << "fully associative" << std::endl;
-    else
-        std::cout << "set associative" << std::endl;
-        
-    std::cout << "\nBlocos\t\tSets" << "\n";
-
-    for (int i = 0; i < numBlocks; i++) {
-        std::cout << i << "\t\t";
-        for (int j = 0; j < numSets; j++) {
-            std::cout << cache[i].sets[j].value << "    ";
-        }
-        std::cout << "\n";
-    }
-
-    std::cout << "\nMemory size:" << memorySize << "\n";
-    std::cout << "Hits: " << numHits << "\n";
-    std::cout << "Misses: " << memorySize -  numHits << "\n"; // num of misses
-    std::cout << "Hit Rate: " << (double)numHits / (memorySize) * 100 << "%\n";
-}
-
-void simulateCache(int numBlocks, int numSets) {
+void simulateCache(int numBlocks, int numSets)
+{
     int memorySize = countFile();
-    int* memory = loadFileToMemory(memorySize);
-    Block* cache = createCache(numBlocks, numSets);
-    
-    int numHits = 0;
-    int setNumber = 0;
+    int *memory = loadFileToMemory(memorySize);
+    Block *cache = createCache(numBlocks, numSets);
 
-    for (int i = 0; i < memorySize; i++) {
+    int numHits = 0;
+    int timeAcess = 0;
+
+    for (int i = 0; i < memorySize; i++)
+    {
         int memoryValue = memory[i];
         int blockIndex = memoryValue % numBlocks;
-        int* cacheValue = &cache[blockIndex].sets[0].value;
+        int *cacheValue = &cache[blockIndex].sets[0].value;
 
-        if (isCacheValue(cache, blockIndex, numSets, memoryValue, setNumber)  && i != 0) {
+        if (isCacheValue(cache, blockIndex, numSets, memoryValue, timeAcess) && i != 0)
+        {
             numHits++;
-        } else {
+        }
+        else
+        {
             cache[blockIndex].sets[0].value = memoryValue;
             cache[blockIndex].sets[0].addr = &memory[i];
-            cache[blockIndex].sets[0].timeAcess = setNumber;
+            cache[blockIndex].sets[0].timeAcess = timeAcess;
+            cache[blockIndex].sets[0].dirtyBit = true;
         }
 
         cache = sortCache(cache, numBlocks, numSets);
-        setNumber++;
+        timeAcess++;
     }
 
     printCache(cache, numBlocks, numSets, memorySize, numHits);
@@ -137,23 +170,48 @@ void simulateCache(int numBlocks, int numSets) {
     delete[] memory;
 }
 
-int main() {
-    int numBlocks = 16;
-    int numSets = 2;
+void Simulator()
+{
+    int numBlocks = 0;
+    int numSets = 0;
 
-    // while (true) {
-    //     std::cout << "Enter the number of blocks: ";
-    //     std::cin >> numBlocks;
-    //     if (powerOfTwo(numBlocks)) break;
-    // }
-
-    // while (true) {
-    //     std::cout << "Enter the number of sets: ";
-    //     std::cin >> numSets;
-    //     if (powerOfTwo(numSets)) break;
-    // }
-
+    while (true)
+    {
+        std::cout << "Enter the number of blocks: ";
+        std::cin >> numBlocks;
+        if (powerOfTwo(numBlocks))
+            break;
+        std::cout << "The number of sets must be a power of two\n";
+    }
+    while (true)
+    {
+        std::cout << "Enter the number of sets: ";
+        std::cin >> numSets;
+        if (powerOfTwo(numSets))
+            break;
+        std::cout << "The number of sets must be a power of two\n";
+    }
     simulateCache(numBlocks, numSets);
+}
+
+int main()
+{
+    int menu = 0;
+    while (menu != 2)
+    {
+        std::cout << "1 - Simulate Cache\n2 - Exit\nOption: ";
+        std::cin >> menu;
+        switch (menu)
+        {
+        case 1:
+            Simulator();
+            break;
+        default:
+            if (menu != 2)
+                std::cout << "Invalid option\n";
+            break;
+        }
+    }
 
     return 0;
 }
